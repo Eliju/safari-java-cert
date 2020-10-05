9fb9c5fc2694e0f62c72f61311d92960d2112734
Simon Roberts <simon@dancingcloudservices.com>
2020-10-05 07:19:57 -0600

    Day 1 Break 1

---
+++ src/main/java/inference/TryThis.java
@@ -0,0 +1,18 @@
+package inference;
+
+public class TryThis {
+  static int x = 10;
+  private int y = 10;
+  public static void main(String[] args) {
+
+//    var x;
+//    x = 10;
+//    static int x = 10;
+//    private int x = 10;
+    
+//    var x = 10;
+//    throw new RuntimeException();
+//    System.out.println("Hello");
+
+  }
+}
